b87d621ac273c6a330bd129daf9ddfab0f5c2a24
wang-sai <532969033@qq.com>
2018-08-08 00:08:15 +0800

    add back component code

--- src/components/common/back.vue
+++ src/components/common/back.vue
@@ -1,17 +1,46 @@
 <template>
-  <div></div>
+  <div>
+    <div class="back-wrapper">
+      <v-image class="back" src="back/cm2_act_view_btn_back_prs@2x.png"></v-image>
+      <div>
+        <text>{{ title }}</text>
+        <text>{{ desc }}</text>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import VImage from './vImage.vue'
+
 export default {
   name: 'back',
-  data () {
-    return {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    desc: {
+      type: String,
+      default: ''
     }
+  },
+  components: {
+    VImage
   },
   methods: {}
 }
 
 </script>
 <style scoped>
+  .back-wrapper {
+    width: 750px;
+    height: 100px;
+    background-color: #000000;
+  }
+
+  .back {
+    width: 24px;
+    height: 44px;
+  }
 </style>
